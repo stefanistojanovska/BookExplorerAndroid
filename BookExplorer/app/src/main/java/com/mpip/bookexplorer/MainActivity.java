16231560c7e7cf9cbc6586556b6671af8f2d715a
stefanistojanovska <stefani.stojanovska@students.finki.ukim.mk>
2020-08-29 16:12:48 +0200

    Menu/toggle added

--- BookExplorer/app/src/main/java/com/mpip/bookexplorer/MainActivity.java
+++ BookExplorer/app/src/main/java/com/mpip/bookexplorer/MainActivity.java
@@ -1,13 +1,44 @@
 package com.mpip.bookexplorer;
 
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    NavigationView nav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.mDrawerLayout);
+        nav = nav = (NavigationView) findViewById(R.id.navView);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                //TODO za sekoj item
+                Toast.makeText(getApplicationContext(), "Hello"+item.toString(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 }
