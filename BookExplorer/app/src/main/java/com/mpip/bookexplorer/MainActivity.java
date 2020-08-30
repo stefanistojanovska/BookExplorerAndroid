1ed221255a30b3ddcd888da0599a93f165791b41
stefanistojanovska <stefani.stojanovska@students.finki.ukim.mk>
2020-08-31 01:20:08 +0200

    Firebase authentication added

--- BookExplorer/app/src/main/java/com/mpip/bookexplorer/MainActivity.java
+++ BookExplorer/app/src/main/java/com/mpip/bookexplorer/MainActivity.java
@@ -1,24 +1,71 @@
 package com.mpip.bookexplorer;
 
+import android.content.Intent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.drawerlayout.widget.DrawerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    List<AuthUI.IdpConfig> providers;
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
     NavigationView nav;
+    Button btnSignOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        providers= Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+        btnSignOut=findViewById(R.id.btn_sign_out);
+       //showSignInOptions();
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance().signOut(MainActivity.this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        btnSignOut.setEnabled(false);
+                        showSignInOptions();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+
+
         drawerLayout = (DrawerLayout) findViewById(R.id.mDrawerLayout);
         nav = nav = (NavigationView) findViewById(R.id.navView);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
@@ -28,11 +75,23 @@ public class MainActivity extends AppCompatActivity {
         nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
-                //TODO za sekoj item
-                Toast.makeText(getApplicationContext(), "Hello"+item.toString(), Toast.LENGTH_SHORT).show();
+                if(item.toString().equals("Log in"))
+                {
+                   // Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                    //startActivity(intent);
+                   // finish();
+                    showSignInOptions();
+                }
+                //Toast.makeText(getApplicationContext(), "Hello"+item.toString(), Toast.LENGTH_SHORT).show();
                 return true;
             }
         });
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).setTheme(R.style.MyTheme).build(),7117);
+
     }
 
     @Override
@@ -40,5 +99,24 @@ public class MainActivity extends AppCompatActivity {
         if(toggle.onOptionsItemSelected(item))
             return true;
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==7117)
+        {
+            IdpResponse response=IdpResponse.fromResultIntent(data);
+            if(resultCode==RESULT_OK)
+            {
+                FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this,user.getEmail(),Toast.LENGTH_SHORT).show();
+                btnSignOut.setEnabled(true);
+            }
+            else
+            {
+                Toast.makeText(this,response.getError().getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
